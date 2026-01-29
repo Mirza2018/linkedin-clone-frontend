c6b6d6cc3f52d8f658db05e8209fa1f638757af8
mirza2018 <mirza.eee.4th@gmail.com>
2026-01-29 08:40:53 +0600

    redux toolkit and redux install

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+const { default: axios } = require("axios");
+
+const clientServer = axios({
+  baseURL: "http://localhost:2018",
+});
---
+++ src/config/redux/store.js
@@ -0,0 +1,14 @@
+//
+//
+//
+// Steps for state Management in redux
+//1. Submit Action.
+//2. Handle Action in it's reducer.
+//3.Register Hrere -> Reducers
+//
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+});
